3d4cec507d61c39a1756bafbfbab0fc8a844b758
Aythami Z. Martin Gonzalez <43406334+Aythamiza@users.noreply.github.com>
2019-01-10 16:00:24 +0000

    First commit

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed= 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moverHorizontal = Input.GetAxis("Horizontal");
+        float moverVertical = Input.GetAxis("Vertical");
+        Vector3 vector = new Vector3(moverHorizontal, 0.0f, moverVertical);
+        rigidbody.AddForce(vector * 4.0f)
+    }
+}
